7efbcbe4adff3348fb22656fcacab5cfef6065a4
LucasRamuus <lucasgabrielpr12@gmail.com>
2025-08-12 23:36:13 -0300

    creation of gateway and repository

---
+++ backend-api/src/main/java/com/achadinhos_cupons/backend_api/domain/gateways/ProductGateway.java
@@ -0,0 +1,17 @@
+package com.achadinhos_cupons.backend_api.domain.gateways;
+
+import com.achadinhos_cupons.backend_api.domain.entities.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductGateway {
+
+    Product save(Product product);
+
+    Optional<Product> findById(Long id);
+
+    List<Product> findAll();
+
+    void deleteById(Long id);
+}
