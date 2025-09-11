14dae7c3630944a7c5c8f4c8b58a0b169c8e6d2e
LucasRamuus <lucasgabrielpr12@gmail.com>
2025-09-10 22:46:59 -0300

    CORREÇÃO

--- backend-api/src/main/java/com/achadinhos_cupons/backend_api/infra/persistence/JpaAdminRepository.java
+++ backend-api/src/main/java/com/achadinhos_cupons/backend_api/infra/persistence/JpaAdminRepository.java
@@ -1,11 +1,14 @@
 package com.achadinhos_cupons.backend_api.infra.persistence;
 
 import com.achadinhos_cupons.backend_api.domain.entities.Admin;
+import com.achadinhos_cupons.backend_api.domain.gateways.AdminGateway;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface JpaAdminRepository extends JpaRepository<Admin, UUID> {
+@Repository
+public interface JpaAdminRepository extends JpaRepository<Admin, UUID>, AdminGateway {
     Optional<Admin> findByUsername(String username);
 }
